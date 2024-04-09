638af216e049db91854eb9497df9f989b3d15040
Murendeni Mukwevho <murendenim@girlcode.co.za>
2024-04-09 19:26:07 +0200

    Upgrade to V4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
+
 
 export default defineConfig({
     integrations: [
+        tailwind(),
         vue(),
         react(),
         svelte(),
