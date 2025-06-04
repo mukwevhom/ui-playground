5ac1cbcb6634f68ebfd9d47125a753943c7ad2f2
Murendeni Mukwevho <mukwevhom@live.com>
2025-06-04 21:36:26 +0200

    feat: enhance Bond Calculator component with improved styling and layout

--- src/components/react/bond-calculator/Calculator.tsx
+++ src/components/react/bond-calculator/Calculator.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 export default function BondCalculator() {
   return (
-    <div>
-      <h1>Bond Calculator</h1>
-      <p>This component will calculate bond prices and yields.</p>
+    <div className='bond-calculator'>
+      <div className='container'>
+        <div>
+          <h1 className='bg-teal-700/60 rounded-4xl inline-block py-1.5 px-2.5 leading-none'>Bond Calculator</h1>
+          <p className='mt-4 text-4xl md:w-1/2'>See how much you'll pay for your bond</p>
+        </div>
+      </div>
       {/* Add your bond calculation logic here */}
     </div>
   );
